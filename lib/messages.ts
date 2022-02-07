da80beb3e3f9b21f1fc606c41c57910df7a51864
kerri crawford <kerriannercrawford+github@gmail.com>
2022-02-07 13:08:22 -0500

    Start of SOAP compat.

--- lib/messages.ts
+++ lib/messages.ts
@@ -2,13 +2,18 @@ import * as uuid from 'uuid';
 import { GenericObject, Message } from './types/global';
 
 export function newMessage(data: GenericObject, attachments?: GenericObject): Message {
-  const msg = {
+  const msg: Message = {
     id: uuid.v4(),
     attachments: attachments || {},
-    data,
     headers: {},
     metadata: {},
   };
+
+  if (process.env.ELASTICIO_PUBLISH_MESSAGES_TO) {
+    msg.body = data;
+  } else {
+    msg.data = data;
+  }
 
   return msg;
 }
--- start.sh
+++ start.sh
@@ -4,6 +4,10 @@
 if [ -n "$ELASTICIO_NODE_EXCHANGE" ]; then
   # if ELASTICIO_NODE_EXCHANGE present, run latest version of ferryman
   node ./node_modules/@openintegrationhub/ferryman/runGlobal.js
+# check whether ELASTIC_
+elif [ -n "$ELASTICIO_PUBLISH_MESSAGES_TO" ]; then
+  ## if ELASTICIO_PUBLISH_MESSAGES_TO present, run sailor 
+  node ./node_modules/elasticio-sailor-nodejs/run.js
 else
   # if ELASTICIO_NODE_EXCHANGE not present, run ferryman version 1.7.0 
   node ./node_modules/@openintegrationhub/ferryman-1-7-0/runGlobal.js
