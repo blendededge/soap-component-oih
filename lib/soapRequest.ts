9a1569f66071eb0b8c35719bbb88b68620b7196a
Kerri Crawford <kerriannercrawford+github@gmail.com>
2022-03-15 11:41:33 -0400

    Updated error handling

--- lib/soapRequest.ts
+++ lib/soapRequest.ts
@@ -15,9 +15,16 @@ export async function processMethod(self: Self, msg: Message, cfg: Config, snaps
     await self.emit('data', msg);
     await self.emit('end');
   } catch (e) {
-    self.logger.info('Error while making request to SOAP Client: ', (e as AxiosError).message);
-    if (cfg.dontThrowErrorFlag) {
-      const msg = newMessage({ errorMessage: (e as AxiosError).message, errorName: (e as AxiosError).name, originalRequest: (e as AxiosError).config.data });
+    self.logger.info('Error while making request to SOAP Client: ', (e as Error).message);
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    if (cfg.dontThrowErrorFlag && (e as any).config) {
+      const err = (e as AxiosError);
+      const msg = newMessage({ errorMessage: err.message, errorName: err.name, originalRequest: err.config.data });
+      await self.emit('data', msg);
+      await self.emit('end');
+    } else if (cfg.dontThrowErrorFlag) {
+      const err = (e as Error);
+      const msg = newMessage({ errorMessage: err.message, errorName: err.name });
       await self.emit('data', msg);
       await self.emit('end');
     } else {
