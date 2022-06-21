c464e6f459187351b813f1a095d5ce31ae96f357
Stephen Hyde <shh2112@gmail.com>
2022-06-21 11:46:07 -0400

    add ferryman extensions wrapper

--- lib/actions/httpRequestAction.ts
+++ lib/actions/httpRequestAction.ts
@@ -1,14 +1,16 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
 import { processMethod } from '../soapRequest';
 import { Config, GenericObject, Message, Self } from '../types/global';
+import { wrapper } from '@blendededge/ferryman-extensions';
 
 async function processAction(this: Self, msg: Message, cfg: Config, snapshot: GenericObject): Promise<void> {
     const self = this;
+    const wrapped = wrapper(self, msg, cfg, snapshot);
     self.logger.debug('msg: ', msg);
     self.logger.debug('cfg: ', cfg);
     self.logger.debug('snapshot :', snapshot);
 
-    return await processMethod(self, msg, cfg, snapshot);
+    return await processMethod(wrapped, msg, cfg, snapshot);
 }
 
 exports.process = processAction;
