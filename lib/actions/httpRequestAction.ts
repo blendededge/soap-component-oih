3ed299c228b0cba0aff63320c396d14929997387
Stephen Hyde <shh2112@gmail.com>
2022-09-09 12:26:04 -0400

    wrap with ferryman extension

--- lib/actions/httpRequestAction.ts
+++ lib/actions/httpRequestAction.ts
@@ -1,9 +1,11 @@
 /* eslint-disable @typescript-eslint/no-this-alias */
+import { wrapper } from '@blendededge/ferryman-extensions';
 import { processMethod } from '../soapRequest';
 import { Config, GenericObject, Message, Self } from '../types/global';
 
 async function processAction(this: Self, msg: Message, cfg: Config, snapshot: GenericObject): Promise<void> {
-    const self = this;
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    const self = wrapper(this, msg as any, cfg, snapshot);
     self.logger.debug('msg: ', msg);
     self.logger.debug('cfg: ', cfg);
     self.logger.debug('snapshot :', snapshot);
