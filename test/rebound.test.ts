9100fea05a8041e52819b3da0f4eba81e320b363
Stephen Hyde <shh2112@gmail.com>
2022-09-14 14:37:49 -0400

    add rate limit tests

--- test/rebound.test.ts
+++ test/rebound.test.ts
@@ -2,6 +2,7 @@ import * as td from 'testdouble';
 import * as nock from 'nock';
 import { processMethod } from '../lib/soapRequest';
 import { Config, Message, Self } from '../lib/types/global';
+import { expect } from 'chai';
 
 const ANY_PARAM = td.matchers.anything();
 let self;
@@ -86,5 +87,47 @@ describe('rebound functionality', () => {
       };
       await processMethod(self, msg, cfg);
       td.verify(self.emit('rebound', ANY_PARAM));
+    });
+
+    it('should not delay end when not rate limited', async () => {
+      const endpoint = nock('https://example.com');
+      endpoint.post('/').reply(200);
+      const msg: Message = {
+        id: '123',
+        attachments: {},
+        data: {},
+        headers: {},
+        metadata: {},
+      };
+      const cfg: Config = {
+        endpointUrl: '\'https://example.com/\'',
+      };
+      const start = Date.now();
+      await processMethod(self, msg, cfg);
+      const delta = Date.now() - start;
+      // there will be some delay in execution time, even when no delay is set
+      // 25 ms is chosen to give plenty of time for execution even on slower machines
+      expect(delta).to.be.lessThanOrEqual(25);
+    });
+
+    it('should delay end when rate limited', async () => {
+      const endpoint = nock('https://example.com');
+      endpoint.post('/').reply(200);
+      const delay = 1000;
+      const msg: Message = {
+        id: '123',
+        attachments: {},
+        data: {},
+        headers: {},
+        metadata: {},
+      };
+      const cfg: Config = {
+        endpointUrl: '\'https://example.com/\'',
+        rateLimitInMs: delay,
+      };
+      const start = Date.now();
+      await processMethod(self, msg, cfg);
+      const delta = Date.now() - start;
+      expect(delta).to.be.greaterThanOrEqual(delay);
     });
 })
